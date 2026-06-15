ac179a2d3aab8af559ccee044761bbcea4e60bcb
yasandu0505 <yasanduyaka2005@gmail.com>
2026-06-16 00:50:43 +0530

    creating the publisher

--- temperature_monitor/temperature_sensor.py
+++ temperature_monitor/temperature_sensor.py
@@ -1,11 +1,22 @@
+#!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
+from std_msgs.msg import Float32
+import random
+ 
 
 class TemperatureSensor(Node):
 
-    def __init__():
+    def __init__(self):
         super().__init__("temperature_sensor")
+        self.get_logger().info("Temperature Monitor Initiated...")
+
+        # using float32 message type and publishes to the /temperature topic
+        self.temperature_publisher = self.create_publisher(Float32, '/temperature', 10)
+        
 
 def main():
     rclpy.init()
+    temperature_sensor_node = TemperatureSensor()
+    rclpy.spin(temperature_sensor_node)
     rclpy.shutdown()
