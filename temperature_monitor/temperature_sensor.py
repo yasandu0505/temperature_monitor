6c00b0ced9c6f1d96871361bc9544b4bfac1f904
yasandu0505 <yasanduyaka2005@gmail.com>
2026-06-16 00:32:49 +0530

    initial commit

---
+++ temperature_monitor/temperature_sensor.py
@@ -0,0 +1,11 @@
+import rclpy
+from rclpy.node import Node
+
+class TemperatureSensor(Node):
+
+    def __init__():
+        super().__init__("temperature_sensor")
+
+def main():
+    rclpy.init()
+    rclpy.shutdown()
